73067c2d4306f050a24e9ce944f56b3b8c7a46cd
Sajidsk7952 <sksajidhussain2003@gmail.com>
2024-09-16 15:41:21 +0530

    converted md files into html and rendered

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,28 +1,75 @@
-// pages/blog/[slug].js
+// pages/data/[slug].js
+import fs from "fs";
+import matter from "gray-matter";
+import { notFound } from "next/navigation";
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import {unified} from 'unified'
+import { transformerCopyButton } from '@rehype-pretty/transformers'
 
-export default function BlogPage({ params }){
-    console.log(params.slug);
-  // data/blogs.js
-const blog =
-  {
-    id: 1,
-    title: "First Blog",
-    description: "This is the first blog description.",
-    slug: "first-blog",
-    author: "Author One",
-    publishedDate: "2024-09-01",
-    image: "https://images.pexels.com/photos/68507/spring-flowers-flowers-collage-floral-68507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    html: "<p>This is the first blog description.</p>"
-  };
+export default async function BlogPage({ params }) {
+  // console.log(params.slug);
+  // data/datas.js
+  // const data =
+  //   {
+  //     id: 1,
+  //     title: "First data",
+  //     description: "This is the first data description.",
+  //     slug: "first-data",
+  //     author: "Author One",
+  //     publishedDate: "2024-09-01",
+  //     image: "https://images.pexels.com/photos/68507/spring-flowers-flowers-collage-floral-68507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //     html: "<p>This is the first data description.</p>"
+  //   };
+  const filepath = `content/${params.slug}.md`;
+  if (!fs.existsSync(filepath)) {
+    return notFound();
+  }
+  const fileContent = fs.readFileSync(filepath, "utf8");
+  const { data, content } = matter(fileContent);
+   
+  const file = await unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeDocument, {title: data.title})
+  .use(rehypeFormat)
+  .use(rehypePrettyCode,{
+    theme : 'github-dark',
+    transformers: [
+      transformerCopyButton({
+        visibility: 'always',
+        feedbackDuration: 3_000,
+      }),
+    ],
+  })
+  .use(rehypeStringify)
+  .process(content);
+  const htmlContent = file.toString();
+  // console.log(data,content);
   return (
     <div className="bg-secondary min-h-screen">
       <div className="max-w-4xl mx-auto p-6 bg-primary-foreground shadow-md rounded-lg">
-        <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
-        <img src={blog.image} alt={blog.title} className="w-full h-auto mb-4 rounded" />
-        <blockquote className="italic text-lg mb-4 border-l-4 border-secondary-foreground px-4">{blog.description}</blockquote>
-        <p className="italic text-gray-600 mb-4">By {blog.author} on {blog.publishedDate}</p>
-        <div className="prose" dangerouslySetInnerHTML={{ __html: blog.html }} ></div>
+        <h1 className="text-3xl font-bold mb-4">{data.title}</h1>
+        <img
+          src={data.image}
+          alt={data.title}
+          className="w-full h-auto mb-4 rounded"
+        />
+        <blockquote className="italic text-lg mb-4 border-l-4 border-secondary-foreground px-4">
+          {data.description}
+        </blockquote>
+        <q className="italic text-gray-600 mb-4">
+          By {data.author} on {data.date}
+        </q>
+        <div
+          className="prose dark:prose-invert max-w-3xl"
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+        ></div>
       </div>
     </div>
   );
-};
+}
